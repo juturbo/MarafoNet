acce76e565e4309df345991688cf36ef5f9cd6cc
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-02-25 21:15:13 +0100

    Added dummy name entry screen component

---
+++ frontend/src/components/NameEntryScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NameEntryScreen() {
+    return (
+        <div className="name-entry-screen">
+            <h1>Name Entry Screen</h1>
+        </div>
+    );
+}
