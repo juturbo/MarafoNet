c1aa12fd48dab848a41b967c8c54650b5bd767b0
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-02-25 13:18:38 +0100

    Added dummy main to server static development page

---
+++ cmd/marafonet/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+const rootPath string = "/"
+const indexPath string = "./frontend/index.html"
+const localFilePath string = "./frontend"
+
+const webSocketPath = "/ws"
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve(rootPath, static.LocalFile(localFilePath, true)))
+
+	router.NoRoute(func(c *gin.Context) {
+		path := c.Request.URL.Path
+		if path == webSocketPath {
+			c.String(404, "Not Found")
+		} else {
+			c.File(indexPath)
+		}
+	})
+	// Start and run the server
+	router.Run(":5000")
+}
