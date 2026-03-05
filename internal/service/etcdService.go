6b510ab3185f1f18c76bc34381c31714c8dc331f
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-03-05 11:44:41 +0100

    feat: implemented atomic put and get from etcd storage

---
+++ internal/service/etcdService.go
@@ -0,0 +1,122 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+const MATCH_COUNTER_PATH = "global/match_counter"
+
+type EtcdService struct {
+	client *clientv3.Client
+}
+
+func NewEtcdService(endpoints []string, dialTimeout time.Duration) (*EtcdService, error) {
+	cfg := clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	}
+	client, err := clientv3.New(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &EtcdService{client: client}, nil
+}
+
+func (etcdService *EtcdService) GetNextMatchID(ctx context.Context) (matchId string, err error) {
+	key := MATCH_COUNTER_PATH
+	for {
+		current, revision, err := etcdService.fetchCurrentAndRevision(ctx, key)
+		if err != nil {
+			return "", err
+		}
+		next := current + 1
+		isUpdateSuccessful, err := etcdService.updateValueIfRevisionMatches(ctx, key, revision, next)
+		if err != nil {
+			return "", err
+		}
+		if isUpdateSuccessful {
+			matchId := fmt.Sprintf("match/%d", next)
+			return matchId, nil
+		}
+	}
+}
+
+func (etcdService *EtcdService) PutNewGame(ctx context.Context, key string, jsonValue []byte) (bool, error) {
+	transaction := etcdService.client.Txn(ctx).
+		If(clientv3.Compare(clientv3.CreateRevision(key), "=", 0)).
+		Then(clientv3.OpPut(key, string(jsonValue)))
+
+	transactionResponse, err := transaction.Commit()
+	if err != nil {
+		return false, err
+	}
+	return transactionResponse.Succeeded, nil
+}
+
+func (etcdService *EtcdService) GetValueAndRevision(ctx context.Context, key string) (matchJson json.RawMessage, revision int64, err error) {
+	response, err := etcdService.client.Get(ctx, key)
+	if err != nil {
+		return matchJson, revision, err
+	}
+	if len(response.Kvs) == 0 {
+		err = fmt.Errorf("key not found: %s", key)
+		return matchJson, revision, err
+	}
+	matchJson = response.Kvs[0].Value
+	revision = response.Kvs[0].ModRevision
+	return matchJson, revision, nil
+}
+
+func (etcdService *EtcdService) PutIfUnmodified(ctx context.Context, key string, jsonValue []byte, lastRevision int64) (bool, error) {
+	transaction := etcdService.client.Txn(ctx).
+		If(clientv3.Compare(clientv3.ModRevision(key), "=", lastRevision)).
+		Then(clientv3.OpPut(key, string(jsonValue)))
+	transactionResponse, err := transaction.Commit()
+	if err != nil {
+		return false, err
+	}
+	return transactionResponse.Succeeded, nil
+}
+
+func (etcdService *EtcdService) Close() error {
+	return etcdService.client.Close()
+}
+
+func (etcdService *EtcdService) fetchCurrentAndRevision(ctx context.Context, key string) (current int, revision int64, err error) {
+	response, err := etcdService.client.Get(ctx, key)
+	if err != nil {
+		return current, revision, err
+	}
+	if len(response.Kvs) == 0 {
+		return current, revision, nil
+	}
+	current, err = strconv.Atoi(string(response.Kvs[0].Value))
+	if err != nil {
+		return current, revision, err
+	}
+	revision = response.Kvs[0].ModRevision
+	return current, revision, nil
+}
+
+func (etcdService *EtcdService) updateValueIfRevisionMatches(ctx context.Context, key string, expectedRevision int64, value int) (bool, error) {
+	var compare clientv3.Cmp
+	if expectedRevision == 0 {
+		compare = clientv3.Compare(clientv3.CreateRevision(key), "=", 0)
+	} else {
+		compare = clientv3.Compare(clientv3.ModRevision(key), "=", expectedRevision)
+	}
+	transaction := etcdService.client.Txn(ctx).
+		If(compare).
+		Then(clientv3.OpPut(key, strconv.Itoa(value)))
+	transactionResponse, err := transaction.Commit()
+	if err != nil {
+		return false, err
+	}
+	return transactionResponse.Succeeded, nil
+}
