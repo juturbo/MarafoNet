1811beaef3c8b1595e20fb4ad0752f688f9dc72d
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-03-08 14:45:43 +0100

    feat: added etcd match watcher

--- internal/service/etcdService.go
+++ internal/service/etcdService.go
@@ -47,10 +47,10 @@ func (etcdService *EtcdService) GetNextMatchID(ctx context.Context) (matchId str
 	}
 }
 
-func (etcdService *EtcdService) PutNewGame(ctx context.Context, key string, jsonValue []byte) (bool, error) {
+func (etcdService *EtcdService) PutNewGame(ctx context.Context, key string, matchJson []byte) (bool, error) {
 	transaction := etcdService.client.Txn(ctx).
 		If(clientv3.Compare(clientv3.CreateRevision(key), "=", 0)).
-		Then(clientv3.OpPut(key, string(jsonValue)))
+		Then(clientv3.OpPut(key, string(matchJson)))
 
 	transactionResponse, err := transaction.Commit()
 	if err != nil {
@@ -73,15 +73,50 @@ func (etcdService *EtcdService) GetValueAndRevision(ctx context.Context, key str
 	return matchJson, revision, nil
 }
 
-func (etcdService *EtcdService) PutIfUnmodified(ctx context.Context, key string, jsonValue []byte, lastRevision int64) (bool, error) {
+func (etcdService *EtcdService) PutUpdatedGameIfRevisionMatch(ctx context.Context, key string, matchJson []byte, lastRevision int64) (bool, error) {
 	transaction := etcdService.client.Txn(ctx).
 		If(clientv3.Compare(clientv3.ModRevision(key), "=", lastRevision)).
-		Then(clientv3.OpPut(key, string(jsonValue)))
+		Then(clientv3.OpPut(key, string(matchJson)))
 	transactionResponse, err := transaction.Commit()
 	if err != nil {
 		return false, err
 	}
 	return transactionResponse.Succeeded, nil
+}
+
+func (etcdService *EtcdService) SetUserCurrentMatchId(ctx context.Context, playerName string, matchId string) error {
+	key := fmt.Sprintf("user/%s/current_match", playerName)
+	_, err := etcdService.client.Put(ctx, key, matchId)
+	return err
+}
+
+func (etcdService *EtcdService) GetUserCurrentMatchId(ctx context.Context, playerName string) (string, error) {
+	key := fmt.Sprintf("user/%s/current_match", playerName)
+	resp, err := etcdService.client.Get(ctx, key)
+	if err != nil || len(resp.Kvs) == 0 {
+		return "", fmt.Errorf("nessuna partita attiva trovata per l'utente")
+	}
+	return string(resp.Kvs[0].Value), nil
+}
+
+func (etcdService *EtcdService) WatchMatch(ctx context.Context, matchId string) (<-chan []byte, context.CancelFunc) {
+	channel := make(chan []byte)
+	watchCtx, cancel := context.WithCancel(ctx)
+	watchChannel := etcdService.client.Watch(watchCtx, matchId)
+	go func() {
+		defer close(channel)
+		for watchResponse := range watchChannel {
+			if watchResponse.Err() != nil {
+				return
+			}
+			for _, event := range watchResponse.Events {
+				if event.Type == clientv3.EventTypePut {
+					channel <- event.Kv.Value
+				}
+			}
+		}
+	}()
+	return channel, cancel
 }
 
 func (etcdService *EtcdService) Close() error {
