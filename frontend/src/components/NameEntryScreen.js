c2162ba3aad3f54a545f9472daae1417aee452a0
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-08 15:08:44 +0100

    Added and styled Name Entry form

--- frontend/src/components/NameEntryScreen.js
+++ frontend/src/components/NameEntryScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './NameEntryScreen.css';
+
 export default function NameEntryScreen() {
+
     return (
         <div className="name-entry-screen">
             <h1>Name Entry Screen</h1>
+            <form>
+                <input type="text" placeholder="Enter your name" />
+                <button type="submit">Join Game</button>
+            </form>
         </div>
     );
 }
