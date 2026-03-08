37ed4d8fff42f60e7c78cf27e85f51c5b16c766e
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-08 17:55:40 +0100

    Updated tests according to new message types

--- internal/networking/envelope_test.go
+++ internal/networking/envelope_test.go
@@ -14,8 +14,8 @@ func initEnvelope(messageType MessageType) *WSEnvelope {
 }
 
 func TestEqualsMessageType(t *testing.T) {
-	envelope := initEnvelope(FirstJoinType)
-	assert.Equal(t, FirstJoinType, envelope.GetMessageType(), "Expected getMessageType() to equal FirstJoinType but got %s", envelope.GetMessageType())
+	envelope := initEnvelope(JoinType)
+	assert.Equal(t, JoinType, envelope.GetMessageType(), "Expected getMessageType() to equal JoinType but got %s", envelope.GetMessageType())
 
 	envelope = initEnvelope(PlayCardType)
 	assert.Equal(t, PlayCardType, envelope.GetMessageType(), "Expected getMessageType() to equal PlayCardType but got %s", envelope.GetMessageType())
