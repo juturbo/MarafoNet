1ee276ffaf2661aceb12b1d325e9acae2e45fe33
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-20 15:55:27 +0100

    Refactored Write and Read functions, partially handled player's first join message

--- internal/networking/WebSocketServer.go
+++ internal/networking/WebSocketServer.go
@@ -1,57 +1,77 @@
 package networking
 
 import (
+	"MarafoNet/internal/service"
+	"context"
 	"encoding/json"
 
 	"github.com/gorilla/websocket"
 )
 
 type WebSocketHub struct {
-	WriteChannel chan json.RawMessage
+	Connection     *websocket.Conn
+	WriteChannel   chan json.RawMessage
+	StorageService *service.EtcdService
+	GameService    *service.GameService
+}
+
+func CreateWebSocketHub(Conn *websocket.Conn, GameService *service.GameService, StorageService *service.EtcdService) WebSocketHub {
+	var hub WebSocketHub
+	hub.Connection = Conn
+	hub.WriteChannel = make(chan json.RawMessage)
+	hub.GameService = GameService
+	hub.StorageService = StorageService
+	return hub
 }
 
 // Calls goroutines to serve read and write channels for one WebSocket connection.
-func ServeWS(Conn *websocket.Conn) {
-	var hub WebSocketHub
-	hub.WriteChannel = make(chan json.RawMessage)
-	go ServeWrite(Conn, hub.WriteChannel)
-	go ServeRead(Conn, hub.WriteChannel)
+func ServeWS(Conn *websocket.Conn, GameService *service.GameService, StorageService *service.EtcdService) {
+	hub := CreateWebSocketHub(Conn, GameService, StorageService)
+	go ServeWrite(hub)
+	go ServeRead(hub)
 
 }
 
-func ServeWrite(Conn *websocket.Conn, writeChannel chan json.RawMessage) {
-	defer Conn.Close()
+func ServeWrite(hub WebSocketHub) {
+	defer hub.Connection.Close()
 
-	for message := range writeChannel {
-		err := Conn.WriteJSON(message)
+	for message := range hub.WriteChannel {
+		err := hub.Connection.WriteJSON(message)
 		if err != nil {
 			return
 		}
 	}
 }
 
-func ServeRead(Conn *websocket.Conn, writeChannel chan json.RawMessage) {
-	defer Conn.Close()
+func ServeRead(hub WebSocketHub) {
+	defer hub.Connection.Close()
 
 	for {
 		var envelope WSEnvelope
-		err := Conn.ReadJSON(&envelope)
+		err := hub.Connection.ReadJSON(&envelope)
 		if err != nil {
 			break
 		}
-		var response, payload = HandleWSEnvelope(envelope)
+		var response, payload = HandleWSEnvelope(envelope, hub)
 		if response {
-			writeChannel <- payload
+			hub.WriteChannel <- payload
 		}
 	}
 }
 
-func HandleWSEnvelope(envelope Envelope) (bool, json.RawMessage) {
+func HandleWSEnvelope(envelope Envelope, hub WebSocketHub) (bool, json.RawMessage) {
 	switch {
 	case envelope.EqualsType(JoinType):
-		panic("unimplemented")
+		var joinPayload JoinPayload
+		json.Unmarshal(envelope.GetPayload(), &joinPayload)
+		gameID, err := hub.StorageService.GetUserCurrentMatchId(context.Background(), joinPayload.PlayerName)
+		if err != nil {
+			// TODO: handle case where user has no active game (go to matchmaking)
+			panic("unimplemented")
+
+		}
+		// TODO: send back JSON game state to user
 	case envelope.EqualsType(PlayCardType):
-		panic("unimplemented")
 	case envelope.EqualsType(SetTrumpType):
 		panic("unimplemented")
 	default:
