ed000e709fb7608e15f997c21998a1a1c3d6629d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-25 10:50:13 +0100

    Added matchmaking's methods signatures

---
+++ internal/matchmaking/MatchmakingHub.go
@@ -0,0 +1,41 @@
+package matchmaking
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type MatchmakingHub struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+// Returns a new Matchmaking hub
+func NewMatchmakingHub() *MatchmakingHub {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &MatchmakingHub{
+		cancel: cancel,
+		ctx:    ctx,
+	}
+}
+
+// Starts the matchmaking service as a Go Routine.
+// It will start to check for players in queue and create games.
+// Can be stopped by calling StopMatchmaking().
+func (hub *MatchmakingHub) StartMatchmaking() {
+
+}
+
+// Stops the matchmaking service.
+func (hub *MatchmakingHub) StopMatchmaking() {
+	hub.cancel()
+}
+
+// Sets a watcher on the requested game, sending the information down the write channel.
+func SetGameWatcher(ctx context.Context, matchId string, writeChannel chan json.RawMessage) error {
+	return nil
+}
+
+func matchmake() {
+
+}
