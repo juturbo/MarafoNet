a110cda7ff92d73a4525e964805f05a7e6405d54
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-26 13:03:41 +0100

    Corrected method call

--- internal/networking/WebSocketServer.go
+++ internal/networking/WebSocketServer.go
@@ -66,7 +66,7 @@ func HandleWSEnvelope(envelope Envelope, hub *websockethub.WebSocketHub) (bool, 
 	case envelope.EqualsType(JoinType):
 		gameID, err := hub.StorageService.GetUserCurrentMatchId(context.Background(), envelope.GetPlayerName())
 		if err != nil {
-			hub.MatchmakingService.SetGameWatcher(
+			hub.SetWatcherCancelFunc(
 				hub.MatchmakingService.JoinQueue(context.Background(), hub.GetPlayerName(), hub.WriteChannel),
 			)
 		} else {
