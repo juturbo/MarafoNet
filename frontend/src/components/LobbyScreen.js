d98173ea0d2778d8c7e858a189bacc920d79ec87
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-31 19:47:19 +0200

    Changed lobby

--- frontend/src/components/LobbyScreen.js
+++ frontend/src/components/LobbyScreen.js
@@ -7,13 +7,7 @@ export default function LobbyScreen() {
     return (
         <div className="lobby-screen">
             <h1>Lobby:</h1>
-            <ListGroup>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-            </ListGroup>
+            <p>Wait while we find players.</p>
         </div>
     );
 }
