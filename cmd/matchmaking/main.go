91258a712d8896f2f20574bf1715e0be7c084cd7
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-04-09 14:57:57 +0200

    Separated matchmaking to a new microservice

---
+++ cmd/matchmaking/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"MarafoNet/internal/matchmaking"
+	"MarafoNet/internal/service"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+const rootPath string = "/"
+
+// FOR LOCAL DEVELOPMENT ONLY: CHANGE WHEN CREATING DOCKER IMAGE
+const indexPath string = "./frontend/build/index.html"
+const localFilePath string = "./frontend/build"
+
+const webSocketPath = "/ws"
+
+var etcdEndpoint = []string{"localhost:2379"}
+
+func main() {
+	printHeader()
+	etcdService, err := service.NewEtcdService(etcdEndpoint, time.Second)
+	if err != nil {
+		log.Fatalf("failed to connect to etcd: %v", err)
+	}
+	defer func() {
+		closeErr := etcdService.Close()
+		if closeErr != nil {
+			log.Printf("failed to close etcd client: %v", closeErr)
+		}
+	}()
+
+	log.Printf("connected to etcd at %v", etcdEndpoint)
+	log.Printf("starting matchmaking services...")
+
+	gameService := service.NewGameService(etcdService)
+	matchMakingService := matchmaking.NewMatchmakingHub(etcdService, gameService)
+	matchMakingService.StartMatchmaking()
+}
+
+func printHeader() {
+	log.Println("====================================")
+	log.Println("        Matchmaking Server          ")
+	log.Println("====================================")
+}
