63708eff78e265a2c4603920596d2482f7f8d572
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-04-10 19:40:33 +0200

    Changed address

--- frontend/src/WebSocketProvider.js
+++ frontend/src/WebSocketProvider.js
@@ -8,9 +8,10 @@ export const WebSocketProvider = ({ children }) => {
   const [isConnected, setIsConnected] = useState(false);
   const [error, setError] = useState(null);
 
+  const address = 'ws://localhost:8080/ws';
   useEffect(() => {
     // Connect to your K8s load balancer
-    ws.current = new WebSocket('ws://localhost:5000/ws');
+    ws.current = new WebSocket(address);
 
     ws.current.onopen = () => {
       setIsConnected(true);
