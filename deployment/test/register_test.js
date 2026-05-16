ed9712c6a45da20cb31ce32ba8b9f9baa86a4355
Federico Bagattoni <federicobagattoni61@gmail.com>
2026-05-16 19:25:07 +0200

    Added k6 stress test for user registration

---
+++ deployment/test/register_test.js
@@ -0,0 +1,41 @@
+import { WebSocket } from 'k6/websockets';
+import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { check } from 'k6';
+import { sleep } from 'k6';
+
+export const options = {
+	iterations: 300,
+	vus: 300,
+	insecureSkipTLSVerify: true,
+};
+
+const payload = {
+  type: "register_user",
+  user: {
+	name: randomString(10),
+	password: randomString(10),
+  },
+}
+
+export default function () {
+	const ws = WebSocket("wss://localhost:8080/ws")
+	ws.addEventListener("open", () => {
+		ws.addEventListener("message", (event) => {
+			let reply = JSON.parse(event.data)
+			check(reply, {
+				"is user registered": (r) => r.type == "register_success"
+			})
+			ws.close()
+		})
+		sleep((Math.random() * 5) + 4)
+		ws.send(JSON.stringify(payload))
+	});
+	
+	ws.addEventListener("error", (error) => {
+		console.log("WebSocket error: " + error.error);
+	});
+
+	ws.addEventListener("close", () => {
+		console.log("Connection closed")
+	});
+}
