307bae2545f387649846a232a3bd9e13f9fc9540
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-05-17 17:22:12 +0200

    feat: Added k6 load testing for registration and scaling

--- deployment/test/register_test.js
+++ deployment/test/register_test.js
@@ -4,30 +4,32 @@ import { check } from 'k6';
 import { sleep } from 'k6';
 
 export const options = {
-	iterations: 300,
 	vus: 300,
+	iterations: 1000,
 	insecureSkipTLSVerify: true,
 };
 
-const payload = {
-  type: "register_user",
-  user: {
-	name: randomString(10),
-	password: randomString(10),
-  },
-}
-
 export default function () {
+	let randomUser = randomString(10)
+	let randomPassword = randomString(10)
+
 	const ws = WebSocket("wss://localhost:8080/ws")
 	ws.addEventListener("open", () => {
 		ws.addEventListener("message", (event) => {
 			let reply = JSON.parse(event.data)
 			check(reply, {
-				"is user registered": (r) => r.type == "register_success"
+				"is user registered": (r) => r.type == "register_success",
+				"register unsuccessful": (r) => r.type == "register_failed",
 			})
 			ws.close()
 		})
 		sleep((Math.random() * 5) + 4)
-		ws.send(JSON.stringify(payload))
+		ws.send(JSON.stringify({
+			type: "register_user",
+			user: {
+				name: randomUser,
+				password: randomPassword,
+			},
+		}))
 	});
 }
